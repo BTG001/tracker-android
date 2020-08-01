bd05121746f4ed86a562dd9c6b11a2005ad31417
BTG001 <brunokipkorir2@gmail.com>
2020-08-01 18:27:12 +0300

    Take the ID of the Button in our layout and returns the view

--- app/src/main/java/com/moringa/mytodolist/MainActivity.java
+++ app/src/main/java/com/moringa/mytodolist/MainActivity.java
@@ -3,12 +3,16 @@ package com.moringa.mytodolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mCreateYourList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCreateYourList = (Button)findViewById(R.id.createYourList);
     }
 }
