511005ebd070b4336e52c07e12fce5cdf8a6b7b1
BTG001 <brunokipkorir2@gmail.com>
2020-08-02 15:18:22 +0300

    Call the setOnClick() method on the button

--- app/src/main/java/com/moringa/mytodolist/ListActivity.java
+++ app/src/main/java/com/moringa/mytodolist/ListActivity.java
@@ -3,6 +3,7 @@ package com.moringa.mytodolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +11,7 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText itemET;
     private Button btn;
     private ListView itemList;
@@ -26,5 +27,12 @@ public class ListActivity extends AppCompatActivity {
         itemET = findViewById(R.id.item_edit_text);
         btn = findViewById(R.id.add_btn);
         itemList = findViewById(R.id.item_list);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
