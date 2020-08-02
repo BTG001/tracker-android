9a29868812f232f8b6dcd8177bfb2d8b1ea4a8d5
BTG001 <brunokipkorir2@gmail.com>
2020-08-02 13:27:01 +0300

    Create a click listener which will be triggered when our button is clicked. The setOnClickListener() method takes a new onClickListener as a parameter

--- app/src/main/java/com/moringa/mytodolist/MainActivity.java
+++ app/src/main/java/com/moringa/mytodolist/MainActivity.java
@@ -3,16 +3,23 @@ package com.moringa.mytodolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mCreateYourList;
+    private Button mCreateYourListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCreateYourList = (Button)findViewById(R.id.createYourList);
+        mCreateYourListButton = (Button)findViewById(R.id.createYourListButton);
+        mCreateYourListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
