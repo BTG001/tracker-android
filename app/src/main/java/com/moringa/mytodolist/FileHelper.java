462a0759ef75b34c8b92e1bce753ad5383ea946d
BTG001 <brunokipkorir2@gmail.com>
2020-08-02 15:53:36 +0300

    Create a method to write on the file that we just created

--- app/src/main/java/com/moringa/mytodolist/FileHelper.java
+++ app/src/main/java/com/moringa/mytodolist/FileHelper.java
@@ -1,4 +1,16 @@
 package com.moringa.mytodolist;
 
+import android.content.Context;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
 public class FileHelper {
+
+    public static final String FILENAME = "listinfo.dat";
+
+    public static void writeData(ArrayList<String> items, Context context) {
+
+        FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+    }
 }
