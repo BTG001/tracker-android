58ca39d76cbeed727396aad99ce75e247d77265b
BTG001 <brunokipkorir2@gmail.com>
2020-08-02 13:32:56 +0300

    Create a new instance of the Intent class such that when we click the mCreateYourListButton a new Intent that takes the user from the MainActivity to the ListActivity will be created and immediately started

--- app/src/main/java/com/moringa/mytodolist/MainActivity.java
+++ app/src/main/java/com/moringa/mytodolist/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringa.mytodolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         mCreateYourListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
             }
         });
     }
