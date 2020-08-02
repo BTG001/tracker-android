ad06b4fcef6af4647ed3b6d6471c46d413c3a4cf
BTG001 <brunokipkorir2@gmail.com>
2020-08-02 15:09:58 +0300

    Create and reference the EditText, Button and ListView in the onCreate Method

--- app/src/main/java/com/moringa/mytodolist/ListActivity.java
+++ app/src/main/java/com/moringa/mytodolist/ListActivity.java
@@ -3,12 +3,22 @@ package com.moringa.mytodolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+    private EditText itemET;
+    private Button btn;
+    private ListView itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        itemET = findViewById(R.id.item_edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemList = findViewById(R.id.item_list);
     }
 }
